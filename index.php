330ca6c49006103afb74df19168c442cad8bda95
FerPM <fer.a.g10b@gmail.com>
2021-04-16 09:05:22 -0500

    project complete

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 <!--llamado al archivo header.php -->
 <?php include("includes/header.php") ?>
 
-    <div class="container p-4">
+    <main class="container p-4">
 
         <div class="row">
 
             <div class="col-md-4">
 
                 <?php if (isset($_SESSION['message'])) {?>
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <?- $_SESSION['message']?>
+                    <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+                    <?= $_SESSION['message']?>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hiden="true">&times;</span>
                     </button>
                     </div>
-                <?php }?>
+                <?php session_unset(); }?>
 
 
                 <div class="card card-body">
@@ -31,13 +31,46 @@
                     </form>
                 </div>
             </div>
+            <div class="col-md-8">
+                <table class="table table-border">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Created at</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        $query = "SELECT * FROM task";
+                        $result_task = mysqli_query($conn,$query);
+
+                        while($row = mysqli_fetch_array($result_task)) {?>
+                        <tr>
+                            <td><?php echo $row['title'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><?php echo $row['created_at'] ?></td>
+                            <td>
+                                <a href="edit.php?id=<?php echo $row['id']?> " class = "btn btn-secondary">
+                                    <i class= "fas fa-marker"></i> 
+                                </a>
+                                <a href="delete_task.php?id=<?php echo $row['id']?>" class = "btn btn-danger">
+                                    <i class= "fas fa-trash-alt"></i> 
+                                </a>
+                            
+                            </td>
+                        </tr>
+
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    
-    </div>
+        
 
-    <div class="col-md-8">
-    
-    </div>
+    </main>
 
+   
 <!--llamado al archivo footer.php -->
 <?php include("includes/footer.php") ?>
--- save_task.php
+++ save_task.php
@@ -1,21 +1,22 @@
 <?php
 
-include("bd.php");
+include('db.php');
 
 if (isset($_POST['save_task'])) {
+  $title = $_POST['title'];
+  $description = $_POST['description'];
+  $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+  $result = mysqli_query($conn, $query);
+  if(!$result) {
+    die("Query Failed.");
+  }
 
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    /* Guadado de datos en Database */
-    $query = "INSERT INTO task (title, description) VALUES('$title', '$description')";
-    $result = mysqli_query($conn, $query);
-   
-    
-    $_SESSION['message'] = 'Task Saved Succesfully';
-    $_SESSION['message_type'] = 'success';
+  $_SESSION['message'] = 'Task Saved succesfully';
+  $_SESSION['message_type'] = 'success';
+
+  header("Location: index.php");
 
 
-    header("Location: index.php ");
 }
 
 ?>
